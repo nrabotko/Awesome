e7310c9f904a4205f189c0643518dfbab0587b7d
Анастасия Работько <arabotko@ad.local>
2020-07-27 17:44:08 +0300

    New styles on App.js

--- App.js
+++ App.js
@@ -21,25 +21,72 @@ import R from './src/resources/R';
 
 const App = () => {
   return (
-      <View 
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        alignSelf: 'center',
-        justifyContent: 'space-between',
-        marginTop: 80,
-        // alignItems: 'center'
-      }}>
+      <View>
+        <View style = {styles.imageStyles} >
+          <Image source = {R.image.newImage}/>
+        </View>
         
-      <Image source = {R.image.newImage}/>
-      <Text source = {R.string.newText}/>
-      
+        <View style = {styles.textalign}>
+          <Text style = {styles.text}>{R.string.newText}</Text>
+        </View>
+
+        <View style = {styles.textalign1}>
+          <View style = {styles.align}>
+            <Text style = {styles.text1}>Пропустить</Text>
+          </View>
+        </View>
+
       </View>   
   );
 };
 
 const styles = StyleSheet.create({
-
+  imageStyles:{
+      flex: 1,
+      flexDirection: 'column',
+      alignSelf: 'center',
+      justifyContent: 'space-between',
+      marginTop: 80,
+      alignItems: 'center'
+  },
+  text: {
+    fontFamily: "Fira Sans Condensed",
+    fontSize: 24,
+    fontWeight: "normal",
+    lineHeight: 29,
+    color: "black",
+    alignItems: 'center',
+    textAlign: 'center',
+    height: 87,
+    marginTop: 460,
+    // ...palette.text, ...{
+    //   marginHorizontal: 8,
+    //   marginVertical: 10
+    // }
+  },
+  textalign: {
+    position: 'absolute',
+    alignSelf: 'center',
+  },
+  textalign1: {
+    marginRight: 92,
+    alignSelf: 'center',
+    marginTop: 780
+  },
+  align: {
+    position: 'absolute',
+    bottom:0
+  },
+  text1: {
+    fontFamily: "Fira Sans Condensed",
+    fontSize: 18,
+    fontWeight: "normal",
+    lineHeight: 22,
+    color: "rgba(0, 0, 0, 0.61)",
+    alignItems: 'center',
+    textAlign: 'center',
+    height: 87
+  },
 });
 
 export default App;
